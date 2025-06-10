4efbeaa5abb85ca890103c389a9b599fea071f9c
fuureya <agiljibrin009@gmail.com>
2025-06-10 10:24:55 +0800

    fix: import getHostInactive function typo import

--- app/index.js
+++ app/index.js
@@ -26,7 +26,7 @@ bot.command("gethostactive", (ctx) => {
 
 bot.command("gethostinactive", (ctx) => {
   ctx.reply("Mengambil Informasi Jumlah Host Dari Zabbix...");
-  getHostsInactive(ctx);
+  getHostInactive(ctx);
 });
 
 bot.command("gettoken", (ctx) => {
--- app/src/telegramController.js
+++ app/src/telegramController.js
@@ -23,7 +23,7 @@ const getHosts = async (ctx) => {
   }
 };
 
-const gethostinactive = async (ctx) => {
+const getHostInactive = async (ctx) => {
   try {
     const message = await getTimeoutHosts();
     await ctx.reply(message);
@@ -47,5 +47,5 @@ export default {
   sendTriggerFourHours,
   getHosts,
   getTokenZabbix,
-  gethostinactive,
+  getHostInactive,
 };
